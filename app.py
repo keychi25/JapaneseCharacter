d56b5688fcc9ceb0351d26c7ee9e55d64141ca71
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-06-18 21:57:04 +0900

    fix : draw

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import cv2
 import keras
 import random
 import string
+import matplotlib.pyplot as plt
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import Sequential
 from keras.models import Sequential
@@ -16,7 +17,6 @@ import pandas as pd
 from flask import Flask, Blueprint, render_template, request, abort, flash, redirect, url_for
 from datetime import datetime
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 app.secret_key = 'hiragana-app'
@@ -57,9 +57,8 @@ def run():
         if not lowerReg.match(j_character):
             flash('ひらがなを入力してください．', category='alert alert-danger')
             return redirect(url_for('index'))
-
-        if file:
-            filename = secure_filename(file.filename)  # ファイル名
+        else:
+            filename = "canvas.png"
             if os.path.exists(tmp_dir):
                 file.save(os.path.join(tmp_dir,
                                        filename))
@@ -68,9 +67,9 @@ def run():
         _, thresh3 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
         X_test = np.array(Image.fromarray(cv2.bitwise_not(thresh3)).resize(
             (img_rows, img_cols), 1), dtype=np.float32)
-        X_test = X_test.astype("float32") / 255
         X_test = X_test.reshape(1, img_rows, img_cols,
                                 1)
+        
         os.remove(os.path.join(tmp_dir,
                                filename))
         y_test.append(0)
@@ -83,16 +82,13 @@ def run():
         result_rank = []
         tmp = list(score[0])
         score = list(score[0])
-        print(score)
         for _ in range(10):
             max_value = max(tmp)
             max_index = score.index(max_value)
-            print(max_index)
             result_rank.append(
                 {"result": classmapping.iloc[max_index].ひらがな,
                  "score": '{:.5%}'.format(max(tmp))}
             )
-
             tmp.remove(max(tmp))
         return render_template('result.html', input_value=j_character, result=result_rank)
     else:
